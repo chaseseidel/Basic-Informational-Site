9f9854ec55528779e01a361b0b951b9b9cb61792
chaseseidel <chaseseidel16@gmail.com>
2023-09-01 10:54:40 -0700

    Write files to work with express

--- index.js
+++ index.js
@@ -1,43 +1,24 @@
-const http = require("http");
-const fs = require("fs/promises");
-
-const host = "localhost";
+import express from "express";
+import { readFile } from "fs/promises";
+const app = express();
 const port = 8080;
 
-const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  switch (req.url) {
-    case "/": {
-      fs.readFile(__dirname + "/index.html").then((data) => {
-        res.writeHead(200);
-        res.end(data);
-      });
-      break;
-    }
-    case "/about": {
-      fs.readFile(__dirname + "/about.html").then((data) => {
-        res.writeHead(200);
-        res.end(data);
-      });
-      break;
-    }
-    case "/contact": {
-      fs.readFile(__dirname + "/contact-me.html").then((data) => {
-        res.writeHead(200);
-        res.end(data);
-      });
-      break;
-    }
-    default: {
-      fs.readFile(__dirname + "/404.html").then((data) => {
-        res.writeHead(404);
-        res.end(data);
-      });
-      break;
-    }
-  }
+app.get("/", async (req, res) => {
+  res.send(await readFile("./index.html", "utf8"));
 });
 
-server.listen(port, host, () => {
-  console.log(`Server is running on http://${host}:${port}`);
+app.get("/about", async (req, res) => {
+  res.send(await readFile("./about.html", "utf8"));
+});
+
+app.get("/contact", async (req, res) => {
+  res.send(await readFile("./contact-me.html", "utf8"));
+});
+
+app.use(async (req, res) => {
+  res.status(404).send(await readFile("./404.html", "utf8"));
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
